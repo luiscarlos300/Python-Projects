1bde78439428eeaae67fefbe0b08a686275cb1a2
Luis Carlos <114296579+luiscarlos300@users.noreply.github.com>
2024-07-16 12:48:41 -0300

    Add files via upload

--- Applications/Renomear_Planilhas.py
+++ Applications/Renomear_Planilhas.py
@@ -4,14 +4,16 @@ import random
 def rename_excel_files(folder_path):
     files = os.listdir(folder_path)
     excel_files = [f for f in files if f.endswith('.xlsx') or f.endswith('.xlsm')]
+
     for i, file_name in enumerate(excel_files, start=1):
         num_random = random.random()
         new_file_name = f'{i}{num_random}.xlsx'
         old_file_path = os.path.join(folder_path, file_name)
         new_file_path = os.path.join(folder_path, new_file_name)
+
         os.rename(old_file_path, new_file_path)
         print(f'Renomeado: {file_name} para {new_file_name}')
 
 
-folder_path = ''
+folder_path = 'C'
 rename_excel_files(folder_path)
